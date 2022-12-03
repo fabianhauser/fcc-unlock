18db2b965924207f14d3e5da2512f95f1464cf03
Fabian Hauser <fabian@fh2.ch>
2022-12-03 14:46:42 +0100

    Update inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs = { nixos-stable.url = "github:NixOS/nixpkgs/nixos-22.05"; };
+  inputs = { nixos-stable.url = "github:NixOS/nixpkgs/nixos-22.11"; };
 
   outputs = { self, nixos-stable, ... }@inputs:
     let
