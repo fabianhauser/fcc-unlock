3b53fefd0f43de3ccbcc4e7f709cd4f46bda7e46
Fabian Hauser <fabian@fh2.ch>
2025-01-16 13:18:19 +0200

    Minor cleanup

--- flake.nix
+++ flake.nix
@@ -1,36 +1,23 @@
 {
   inputs = {
-    nixos-stable.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs =
-    { self, nixos-stable, ... }@inputs:
+    { self, nixpkgs, ... }@inputs:
     let
       system = "x86_64-linux";
-      pkgs = import nixos-stable { inherit system; };
+      pkgs = import nixpkgs { inherit system; };
     in
     {
-      # Executed by `nix flake check`
-      checks."<system>"."<name>" = derivation;
-      # Executed by `nix build .#<name>`
-      packages."<system>"."<name>" = derivation;
-      # Executed by `nix build .`
       packages.${system}.default = pkgs.stdenv.mkDerivation {
         name = "fcc-unlock";
-
-        # Source Code
-        # See: https://nixos.org/nixpkgs/manual/#ssec-unpack-phase
         src = ./src;
-
-        # Dependencies
-        # See: https://nixos.org/nixpkgs/manual/#ssec-stdenv-dependencies
         buildInputs = with pkgs; [
           coreutils
           gcc
         ];
 
-        # Build Phases
-        # See: https://nixos.org/nixpkgs/manual/#sec-stdenv-phases
         configurePhase = ''
           declare -xp
           sed -e "s#LIBDIR#$out/lib#" "$src/fcc-unlock.c" > fcc-unlock-patched.c
